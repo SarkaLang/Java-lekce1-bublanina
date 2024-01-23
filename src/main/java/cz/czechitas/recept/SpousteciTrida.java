d59fa3204ae92097afb30285d9e334d2a3a7934a
SarkaLang <sarka.langy@gmail.com>
2024-01-23 11:15:58 +0100

    Upečeno.

--- src/main/java/cz/czechitas/recept/SpousteciTrida.java
+++ src/main/java/cz/czechitas/recept/SpousteciTrida.java
@@ -49,7 +49,39 @@ public class SpousteciTrida {
     // Pouzivejte napovidani v editoru.
     // Vyskakuje samo nebo pomoci Ctrl+Mezernik
 
+    for (int i = 0; i < 4; i++) {
     cervenaMiska.nalozSiJedenKus(vajicka);
-  }
+    }
 
+    cervenaMiska.nalozSiCelyObsah(pytlikCukru);
+    mixer.zamichej(cervenaMiska);
+
+    cervenaMiska.nalozSiCelyObsah(maslo125g);
+    mixer.zamichej(cervenaMiska);
+
+   kuchynskaVaha.vynulujSeS(zlutaMiska);
+   while (kuchynskaVaha.zjistiHmotnost(zlutaMiska) != 250) {
+     if(kuchynskaVaha.zjistiHmotnost(zlutaMiska) > 250) {
+       zlutaMiska.vylozSiTrochu();
+     } else {
+       zlutaMiska.nalozSiTrochu(pytlikMouky);
+     }
+   }
+    cervenaMiska.nalozSiObsahJineMisky(zlutaMiska);
+    cervenaMiska.nalozSiCelyObsah(prasekDoPeciva);
+    mixer.zamichej(cervenaMiska);
+
+    plech.preberSiObsah(cervenaMiska);
+
+    for (int i = 0; i < 50; i++) {
+      plech.posypSeKusem(ovoce);
+    }
+
+    trouba.zapniSe(180);
+    trouba.nechejPect(5);
+    trouba.vlozSiDovnitr(plech);
+    trouba.nechejPect(25);
+    trouba.vypniSe();
+    trouba.vyndejObsahVen();
+  }
 }
